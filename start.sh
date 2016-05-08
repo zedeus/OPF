9fda5d089ff07de40ee420020cc3a1ff2b529a60
Zed <zedeus@pm.me>
2016-05-08 15:11:37 +0200

    Fix start script.

--- start.sh
+++ start.sh
@@ -37,13 +37,10 @@ set -x
 
 cd $(dirname $(readlink -f $0))
 
-source ./config.txt
-
 fn_stop ()
 { # This is function stop
    sudo killall raspimjpeg
    sudo killall php
-   sudo killall motion
 }
 
 #start operation
@@ -52,8 +49,3 @@ sudo mkdir -p /dev/shm/mjpeg
 sudo chown www-data:www-data /dev/shm/mjpeg
 sudo chmod 777 /dev/shm/mjpeg
 sleep 1;sudo su -c 'raspimjpeg > /dev/null &' www-data
-if [ -e /etc/debian_version ]; then
-   sleep 1;sudo su -c "php /var/www/$rpicamdir/schedule.php > /dev/null &" www-data
-else
-   sleep 1;sudo su -c '/bin/bash' -c "php /var/www/$rpicamdir/schedule.php > /dev/null &" www-data
-fi
